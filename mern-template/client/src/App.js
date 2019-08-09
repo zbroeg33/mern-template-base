2d8fe031b4d25cbab4012ca5a0d8885204d52a43
Zacharybroeg <Zacharybroeg@u.boisestate.edu>
2019-08-08 22:16:02 -0600

    redid folder sturcture to seperate out things better
    will add mocha and chai in for testing as well as node-sass at next commit

--- mern-template/client/src/App.js
+++ mern-template/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './scss/App.scss';
+import './App.css';
 
 function App() {
   return (
---
+++ mern-template/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://user:user1@cluster0-kuhbz.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
